b8b13473906e8cd08437166ff2b83610308b62f6
Magdiel Rotta Almeida <rotta.aero@gmail.com>
2023-12-14 19:45:12 -0300

    feat: projeto prático 1 criado - to do list

--- comunicacao-componentes/src/app/app.component.ts
+++ comunicacao-componentes/src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  public addValue: number = 0;
+
+  constructor(){
+
+  }
+
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
   title = 'comunicacao-componentes';
 }
