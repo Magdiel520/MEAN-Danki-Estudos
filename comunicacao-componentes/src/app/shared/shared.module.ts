8fadd8f36ee58ef096863b5530cd70ef0be6c04c
Magdiel Rotta Almeida <rotta.aero@gmail.com>
2023-12-14 12:56:48 -0300

    feat:criação do submódulo dentro de shared

--- comunicacao-componentes/src/app/shared/shared.module.ts
+++ comunicacao-componentes/src/app/shared/shared.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NewComponentComponent } from './new-component/new-component.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    NewComponentComponent
+  ],
   imports: [
     CommonModule
   ]
