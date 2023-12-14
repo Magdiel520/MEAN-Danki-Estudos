af775fdc454531c7d65fb7e7555c68ef37ee82c9
Magdiel Rotta Almeida <rotta.aero@gmail.com>
2023-12-14 18:46:42 -0300

    feat: criação do componente output

--- comunicacao-componentes/src/app/shared/shared.module.ts
+++ comunicacao-componentes/src/app/shared/shared.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NewComponentComponent } from './new-component/new-component.component';
 import { InputComponent } from './input/input.component';
+import { OutputComponent } from './output/output.component';
 
 
 
 @NgModule({
   declarations: [
     NewComponentComponent,
-    InputComponent
+    InputComponent,
+    OutputComponent
   ],
   exports:[
     NewComponentComponent,
