64c468046df3aac80cde697fd8b3ee6a3bf1d9ec
Magdiel Rotta Almeida <rotta.aero@gmail.com>
2023-12-18 21:49:51 -0300

    feat: instalação do cypress e lint

--- projeto-pratico-1/src/app/app.component.spec.ts
+++ projeto-pratico-1/src/app/app.component.spec.ts
@@ -19,17 +19,4 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
-
-  it(`should have as title 'projeto-pratico-1'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('projeto-pratico-1');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('projeto-pratico-1 app is running!');
-  });
 });
